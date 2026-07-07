394d672fd81c70524ce447ffdbefb386da8862f6
岡田雄大 <yudai@okadayuudainoMacBook-Air.local>
2026-07-08 05:18:54 +0900

    [Add] Titleシーンの基盤作成。

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,25 +1,15 @@
 using UnityEngine;
 
-/// <summary>
-/// ゲーム全体で共有する情報を管理するクラス
-/// シーンをまたいで保持したい情報をここで管理する。
-/// </summary>
 public class GameManager : MonoBehaviour
 {
-    // シングルトン
     public static GameManager Instance { get; private set; }
 
-    [Header("プレイヤー情報")]
-    [Tooltip("チーズを取得しているか")]
-    public bool HasCheese = false;
+    public bool HasCheese { get; set; }
 
-    [Header("シーン遷移")]
-    [Tooltip("次のシーンで出現するSpawnPointのID")]
-    public string NextSpawnPointID = "";
+    public string NextSpawnPointID { get; set; }
 
     private void Awake()
     {
-        // 既にGameManagerが存在するなら自分を削除
         if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
@@ -27,14 +17,9 @@ public class GameManager : MonoBehaviour
         }
 
         Instance = this;
-
-        // シーンが切り替わっても消えない
         DontDestroyOnLoad(gameObject);
     }
 
-    /// <summary>
-    /// ゲーム開始時の状態へ戻す
-    /// </summary>
     public void ResetGame()
     {
         HasCheese = false;
