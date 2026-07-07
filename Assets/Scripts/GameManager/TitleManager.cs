c75d769b7c163e6f9a4c965a6b115c830e017a7e
岡田雄大 <yudai@okadayuudainoMacBook-Air.local>
2026-07-08 05:32:14 +0900

    [Add] TitleManagerの作成。

---
+++ Assets/Scripts/GameManager/TitleManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class TitleManager : MonoBehaviour
+{
+    private void Update()
+    {
+        bool start =
+            Mouse.current != null &&
+            Mouse.current.leftButton.wasPressedThisFrame;
+
+        if (Keyboard.current != null)
+        {
+            start |= Keyboard.current.enterKey.wasPressedThisFrame;
+            start |= Keyboard.current.spaceKey.wasPressedThisFrame;
+        }
+
+        if (!start)
+            return;
+
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.ResetGame();
+        }
+
+        SceneManager.LoadScene("Shelter");
+    }
+}
